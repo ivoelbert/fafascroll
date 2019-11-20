f671d1464c77baf011f51dbf55b9dfcdc8432eec
ivoelbert <ivo.elbert1@gmail.com>
2019-11-20 16:59:53 -0300

    Remove Closer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { ScrollerPart } from './Components/ScrollerPart/ScrollerPart';
-import { Closer } from './Components/Closer/Closer';
 import './App.scss';
 
 const App: React.FC = () => {
     return (
         <div>
             <ScrollerPart />
-            <Closer />
         </div>
     );
 };
