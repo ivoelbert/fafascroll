e206fd8358eca2a994533c915af612b0bdae47f3
ivoelbert <ivo.elbert1@gmail.com>
2019-11-20 13:22:21 -0300

    Updates

---
+++ precache-manifest.d7dd7a9a968a95d288e959efaed1ea2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0876ac6c0b262fc018b24c3216568445",
+    "url": "/fafascroll/index.html"
+  },
+  {
+    "revision": "191e5097ec346f26f438",
+    "url": "/fafascroll/static/css/main.70f68ec1.chunk.css"
+  },
+  {
+    "revision": "62317eacb60ed884b56a",
+    "url": "/fafascroll/static/js/2.b794afde.chunk.js"
+  },
+  {
+    "revision": "191e5097ec346f26f438",
+    "url": "/fafascroll/static/js/main.76553d62.chunk.js"
+  },
+  {
+    "revision": "af82141fdbe6a675047b",
+    "url": "/fafascroll/static/js/runtime-main.848b7233.js"
+  }
+]);
